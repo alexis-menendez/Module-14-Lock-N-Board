d61eecae97d1b433819865ec83e4b33f3ee4c3e1
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-22 22:29:18 -0500

    got the user seeds working!!!

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -4,14 +4,17 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'Test1', password: '1234' },
-    { username: 'Alexis1', password: '1234' },
-    { username: 'Divina1', password: '1234' },
     { username: 'Test', password: 'password' },
     { username: 'Alexis', password: 'password' },
     { username: 'Divina', password: 'password' },
-    { username: 'JollyGuru', password: 'password' },
-    { username: 'SunnyScribe', password: 'password' },
-    { username: 'RadiantComet', password: 'password' },
+    { username: 'User', password: 'password' },
+    { username: 'Test1', password: '1234' },
+    { username: 'Alexis1', password: '1234' },
+    { username: 'Divina1', password: '1234' },
+    { username: 'User1', password: '1234' },
+    { username: 'Test2', password: 'open' },
+    { username: 'Alexis2', password: 'open' },
+    { username: 'Divina2', password: 'open' },
+    { username: 'User2', password: 'open' },
   ], { individualHooks: true });
 };
