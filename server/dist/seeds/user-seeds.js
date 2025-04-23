f2776e488d9ecae3b8e127b353f40277fb5397aa
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-22 22:22:49 -0500

    trying to get stupid login to work

--- server/dist/seeds/user-seeds.js
+++ server/dist/seeds/user-seeds.js
@@ -2,6 +2,9 @@
 import { User } from '../models/user.js';
 export const seedUsers = async () => {
     await User.bulkCreate([
+        { username: 'Test', password: '1234' },
+        { username: 'Alexis', password: '1234' },
+        { username: 'Divina', password: '1234' },
         { username: 'JollyGuru', password: 'password' },
         { username: 'SunnyScribe', password: 'password' },
         { username: 'RadiantComet', password: 'password' },
