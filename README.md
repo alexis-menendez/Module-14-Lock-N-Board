303becb5eb423d2b3a78bde298691f4020cf1582
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-22 22:46:25 -0500

    walkthrough video
    added link into readme, too tired to make readme tonight will do in the morning

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Module-14-lock-n-board
+
+walkthrough video link: https://drive.google.com/file/d/1EKAMk0dPdx7ZgTJARjjorZ_RJB6NCyJ9/view?usp=drive_link
--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -8,17 +8,14 @@ export const seedUsers = async () => {
     { username: 'user', password: 'user' },
     { username: 'alexis', password: 'alexis' },
     { username: 'divina', password: 'divina' },
-
     { username: 'Test0', password: 'password' },
     { username: 'User0', password: 'password' },
     { username: 'Alexis0', password: 'password' },
     { username: 'Divina0', password: 'password' },
-
     { username: 'Test1', password: '1234' },
     { username: 'User1', password: '1234' },
     { username: 'Alexis1', password: '1234' },
     { username: 'Divina1', password: '1234' },
-  
     { username: 'Test2', password: 'open' },
     { username: 'User2', password: 'open' },
     { username: 'Alexis2', password: 'open' },
