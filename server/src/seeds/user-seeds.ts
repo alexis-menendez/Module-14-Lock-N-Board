64a9693d4471fe4846bd52ab03dcb385316807f3
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-22 22:31:08 -0500

    adding users

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -4,17 +4,25 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'Test', password: 'password' },
-    { username: 'Alexis', password: 'password' },
-    { username: 'Divina', password: 'password' },
-    { username: 'User', password: 'password' },
+    { username: 'test', password: 'test' },
+    { username: 'user', password: 'user' },
+    { username: 'alexis', password: 'alexis' },
+    { username: 'divina', password: 'divina' },
+
+    { username: 'Test0', password: 'password' },
+    { username: 'User0', password: 'password' },
+    { username: 'Alexis0', password: 'password' },
+    { username: 'Divina0', password: 'password' },
+
     { username: 'Test1', password: '1234' },
+    { username: 'User1', password: '1234' },
     { username: 'Alexis1', password: '1234' },
     { username: 'Divina1', password: '1234' },
-    { username: 'User1', password: '1234' },
+  
     { username: 'Test2', password: 'open' },
+    { username: 'User2', password: 'open' },
     { username: 'Alexis2', password: 'open' },
     { username: 'Divina2', password: 'open' },
-    { username: 'User2', password: 'open' },
+
   ], { individualHooks: true });
 };
