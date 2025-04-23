f17dfd3075121c188922b48abc30234a0c6e1b9e
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-22 22:26:27 -0500

    testing users

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -4,9 +4,12 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'Test', password: '1234' },
-    { username: 'Alexis', password: '1234' },
-    { username: 'Divina', password: '1234' },
+    { username: 'Test1', password: '1234' },
+    { username: 'Alexis1', password: '1234' },
+    { username: 'Divina1', password: '1234' },
+    { username: 'Test', password: 'password' },
+    { username: 'Alexis', password: 'password' },
+    { username: 'Divina', password: 'password' },
     { username: 'JollyGuru', password: 'password' },
     { username: 'SunnyScribe', password: 'password' },
     { username: 'RadiantComet', password: 'password' },
